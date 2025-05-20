e9dcf4c143830bda69594e52bf85136bab32b2ea
Esteban-ignacio <163415352+Esteban-ignacio@users.noreply.github.com>
2025-05-19 22:23:05 -0400

    cajas de texto agregados en editar, agregar y eliminar un productos

--- maestranza/src/app/pages/listar-productos/listar-productos.page.ts
+++ maestranza/src/app/pages/listar-productos/listar-productos.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonLabel, IonItem, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-listar-productos',
   templateUrl: './listar-productos.page.html',
   styleUrls: ['./listar-productos.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonItem, IonLabel, IonInput, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ListarProductosPage implements OnInit {
 
