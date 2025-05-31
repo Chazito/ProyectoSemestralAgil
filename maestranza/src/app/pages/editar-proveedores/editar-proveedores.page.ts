8fbde21051d8c6b3501242b0fa1108f5961dcdcf
Esteban-ignacio <163415352+Esteban-ignacio@users.noreply.github.com>
2025-05-31 15:35:50 -0400

    datos en pagina editar proveedores

--- maestranza/src/app/pages/editar-proveedores/editar-proveedores.page.ts
+++ maestranza/src/app/pages/editar-proveedores/editar-proveedores.page.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonList, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-proveedores',
   templateUrl: './editar-proveedores.page.html',
   styleUrls: ['./editar-proveedores.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonLabel, IonItem, IonButton, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class EditarProveedoresPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  cancelar() {
+    this.router.navigate(['/vista-inventario']);
   }
 
 }
